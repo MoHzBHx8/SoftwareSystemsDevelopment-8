446c37b6c3e0de31b95e2d243a8fa20c72b9d1fb
test <test@test.test>
2026-01-05 12:23:04 +0900

    added header

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -19,7 +19,7 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
-            <Header message="Hello World" />
+            <Header message="My GitHub username is MoHzBHx8" />
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
